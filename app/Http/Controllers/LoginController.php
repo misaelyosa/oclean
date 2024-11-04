9c7f9aaa4840aa136fe9095abb143c40e9db9f7b
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-11-04 15:12:51 +0700

    create : login page + controller
    initial controller with auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request){
+        $data=$request->validate([
+            'email'=>'required|email:dns',
+            'password'=>'required',
+        ]);
+
+        if(Auth::attempt($data)){
+            $request->session()->regenerate();
+            return redirect()->intended('/home');
+        }
+        else {
+            return back()->withErrors([
+                'email'=>'Login Failed',
+            ])->withInput();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function() {
+    return view('login.index');
+});
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/home', function () {
+    return view('home');
 });
