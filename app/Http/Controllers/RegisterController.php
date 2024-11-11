7b94384ff852bf4d6f9901ee5e1ef705bb9d128c
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-11-11 16:59:20 +0700

    fix : register fixed

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,27 +8,39 @@ use App\Models\Roles;
 class RegisterController extends Controller
 {
     public function store(Request $request){
-        dd('ballz');
         $validatedData = $request->validate([
             'name' => 'required|max:255',
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|confirmed',
             'alamat' => 'required|max:255',
-            'no_telp' => 'required|max:12|min:11',
+            'no_telp' => 'required|max:14|min:11',
             'umur' => 'required|max:2',
             'gender' => 'required',
             'role' => 'required',
         ]);
+        
 
         $validatedData['password'] = bcrypt($validatedData['password']);
        
         $user = User::create($validatedData);
 
-        Roles::create([
-            'user_id' => $user->id,
-            'role' => $validatedData['role'],
-        ]);
-        
+        $role = null;
+
+        if($validatedData['role'] === 'User'){
+            $role = 'user';
+        } elseif ($validatedData['role'] === 'Peternak Maggot'){
+            $role = 'peternak_maggot';
+        } elseif ($validatedData['role'] === 'Bank Sampah'){
+            $role = 'admin_bank_sampah';
+        }
+
+        if($role!=null){
+            Roles::create([
+                'user_id' => $user->id,
+                'role' => $role,
+            ]);
+        }
+        // dd([$validatedData , $role]);
 
         return redirect('/login')->with('success', 'User Berhasil Ditambahkan, Silahkan Login');
     }
