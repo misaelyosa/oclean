e707f678abeb131963039b265f8c70dab7c27d9c
Dohazo <pootisspy931@gmail.com>
2024-11-22 12:01:37 +0700

    styleing

--- resources/views/navbar/nav.blade.php
+++ resources/views/navbar/nav.blade.php
@@ -4,7 +4,7 @@
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
   <a href="" class="flex items-center space-x-2 rtl:space-x-reverse">
       <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
-      <span class="self-center text-2xl text-center font-semibold whitespace-nowrap dark:text-white">Welcome, {{auth()->user()->name}} !</span>
+      <span class="self-center text-2xl text-center font-semibold whitespace-nowrap dark:text-white">Welcome,  !</span>
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
     
@@ -21,13 +21,13 @@
         <a href="#" class="block py-2 px-3 {{$title === "home" ? " rounded text-white md:text-green-700 hover:text-green-700  dark:text-gray-950   md:dark:text-green-500" : "text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent   dark:text-gray-950  md:hover:text-green-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"}}">Home</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent   dark:text-gray-950  md:hover:text-green-700  md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Shop</a>
+        <a href="#" class="block py-2 px-3 {{$title === "shop" ? " rounded text-white md:text-green-700 hover:text-green-700  dark:text-gray-950   md:dark:text-green-500" : "text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent   dark:text-gray-950  md:hover:text-green-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"}}">Shop</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent  dark:text-black  md:hover:text-green-700  md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Penjemputan Sampah</a>
+        <a href="#" class="block py-2 px-3 {{$title === "sampah" ? " rounded text-white md:text-green-700 hover:text-green-700  dark:text-gray-950   md:dark:text-green-500" : "text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent   dark:text-gray-950  md:hover:text-green-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"}}">Penjemputan Sampah</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent dark:text-black md:hover:text-green-700 m md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Customer Service</a>
+        <a href="#" class="block py-2 px-3 {{$title === "customer" ? " rounded text-white md:text-green-700 hover:text-green-700  dark:text-gray-950   md:dark:text-green-500" : "text-gray-900 rounded hover:bg-green-700 md:hover:bg-transparent   dark:text-gray-950  md:hover:text-green-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"}}">Customer Service</a>
       </li>
       <li>
       @if (Auth::check())
