87781c40949258cbdb32f08a6ca3ef85d0e9d045
847980 <137378009+847980@users.noreply.github.com>
2024-12-17 19:54:28 +0700

    done

--- resources/views/admin_bank_sampah/index.blade.php
+++ resources/views/admin_bank_sampah/index.blade.php
@@ -20,7 +20,7 @@
                 <tbody>
                     @foreach ($banks as $bank)
                         <tr
-                            class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            class="odd:bg-white odd:dark:bg-gray-900 even:bg-green-50 even:dark:bg-green-800 border-b dark:border-gray-700">
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ $bank->nama }}
