4ac66468902c0957ca619bf6f31da5e8509f7e56
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-18 11:03:42 +0700

    create home landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,59 @@
-@extends('base.base')
-@include('navbar.nav')
-@section('content')
-    
-    <h1 class="font-black font-inter text-5xl">ini home</h1>
+@extends('base.homebase')
 
-    @if (Auth::check())
-        <h1>Welcome, {{ Auth::user()->name }}</h1>
-        <form action="/logout" method="post">
-            @csrf
-            <button type="submit" class="bg-red-500 hover:bg-red-700 text-white">Logout</button>
-        </form>
-    @else
-        <button> <a href="/login">Login</a></button>
-    @endif
+@section('content')
+    <header>
+        <div>O-Clean</div>
+        <div>O-Clean</div>
+        @if (Auth::check())
+            <div><a>Welcome,  {{ Auth::user()->name }}</a></div>
+        @else
+            <div><a href="/login">Login</a></div>
+        @endif
+    </header>
+    <section class="first">
+        <div class="outer">
+            <div class="inner">
+                <div class="bg-home">
+                    <h2 class="section-heading">Scroll down</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="second">
+        <div class="outer">
+            <div class="inner">
+                <div class="bg-home">
+                    <h2 class="section-heading">Animated with GSAP</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="third">
+        <div class="outer">
+            <div class="inner">
+                <div class="bg-home">
+                    <h2 class="section-heading">GreenSock</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="fourth">
+        <div class="outer">
+            <div class="inner">
+                <div class="bg-home">
+                    <h2 class="section-heading">Animation platform</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="fifth">
+        <div class="outer">
+            <div class="inner">
+                <div class="bg-home">
+                    <h2 class="section-heading">Keep scrolling</h2>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
+
